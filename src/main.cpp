9076bf897add5eb9d9fcebecedddd35fd86ade15
Yusuf Kelany <me@yusufyaser.xyz>
2025-02-12 10:49:44 +0200

    set max memory based on system ram

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,13 @@
 #undef APIENTRY
 #include <Windows.h>
 #endif
-#ifdef __linux__
+#if defined(__linux__) || defined(__APPLE__)
 #include <sys/resource.h>
+#include <sys/sysinfo.h>
+#endif
+#ifdef __APPLE__
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #endif
 
 inline bool ctrlC = false;
@@ -29,7 +34,7 @@ size_t getMaxMemory() {
 	return maxMemory;
 }
 
-inline char* emergencyMemory;
+inline void* emergencyMemory;
 inline std::atomic<bool> oom = false;
 inline std::thread::id mainThread;
 
@@ -37,9 +42,9 @@ void oomHandler() {
 	std::thread::id id = std::this_thread::get_id();
 	if (!oom.load()) free(emergencyMemory);
 	if (id == mainThread) {
-		std::cerr << id << " (main): Out of game memory" << std::endl;
+		std::cerr << "Err (" << id << " (main)) -> Out of Game Memory" << std::endl;
 	} else {
-		std::cerr << id << ": Out of game memory" << std::endl;
+		std::cerr << "Err (" << id << ") -> Out of Game Memory" << std::endl;
 	}
 	if (oom.load()) {
 		if (mainThread != id) {
@@ -53,8 +58,6 @@ void oomHandler() {
 #endif
 	if (mainThread != id) {
 		std::this_thread::sleep_for(std::chrono::seconds(999));
-	} else {
-		std::abort();
 	}
 }
 
@@ -73,11 +76,17 @@ int main(int argc, char* argv[]) {
 	std::signal(SIGINT, signalHandler);
 	emergencyMemory = (char*)malloc(1024 * 1024 * 16);
 	mainThread = std::this_thread::get_id();
+	std::set_new_handler(oomHandler);
 
 #ifdef _WIN32
-	maxMemory = 4294967296;
+	MEMORYSTATUSEX memInfo;
+	memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+	if (!GlobalMemoryStatusEx(&memInfo)) {
+		error("Failed to set memory limit");
+		return 1;
+	}
 
-	std::set_new_handler(oomHandler);
+	maxMemory = memInfo.ullTotalPhys / 2;
 
 	HANDLE job = CreateJobObject(NULL, NULL);
 	if (job == NULL) {
@@ -98,8 +107,24 @@ int main(int argc, char* argv[]) {
 	}
 #endif
 
-#ifdef __linux__
-	maxMemory = 4294967296;
+#if defined(__linux__) || defined(__APPLE__)
+#if defined(__linux__)
+	struct sysinfo sysInfo;
+	if (sysinfo(&sysInfo) != 0) {
+		error("Failed to set memory limit");
+		return 1;
+	}
+
+	maxMemory = sysInfo.totalram / 2;
+#elif defined(__APPLE__)
+	size_t size = sizeof(maxMemory);
+	if (sysctlbyname("hw.memsize", &maxMemory, &size, nullptr, 0) != 0) {
+		error("Failed to set memory limit");
+		return 1;
+	}
+
+	maxMemory = maxMemory / 2;
+#endif
 
 	rlimit limit;
 	limit.rlim_cur = maxMemory;
