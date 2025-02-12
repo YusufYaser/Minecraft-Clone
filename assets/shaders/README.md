c1af63326139ff05fd655c35b07537b379600c3c
Yusuf Kelany <me@yusufyaser.xyz>
2025-02-12 15:13:32 +0200

    fix 2 oopsies

--- assets/shaders/README.md
+++ assets/shaders/README.md
@@ -11,6 +11,8 @@ ShaderName
 You don't need to have both the vertex and fragment shader at the same time.
 
 Game Shaders: `PostProcessing`, `DefaultShader`, `GUI`, `Skybox`\
-You can copy the default shaders from [the source code](/src/Shaders).
+You can copy the default shaders from [the source code](/src/Shaders) here
+> [!WARNING]  
+> You must remove the `R"END(` and `)END"` from the shaders' source codes after copying them to the `assets/shaders` directory
 
 You can reload the game shaders using `/ + U`
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -84,7 +84,7 @@ public:
 	float getWorldResolution() const { return m_worldRes; };
 	void setWorldResolution(float newWorldRes) { m_worldRes = newWorldRes; };
 
-	void loadWorld(WorldSettings& settings, glm::vec3 playerPos = { 0, 0, 0 }, glm::vec3 playerOrientation = { 0, 0, 0 }, bool playerFlying = false);
+	void loadWorld(WorldSettings& settings, glm::vec3 playerPos = { 0, 0, 0 }, glm::vec3 playerOrientation = { 1, 0, 0 }, bool playerFlying = false);
 	void unloadWorld();
 	bool loadingWorld() const { return m_loadingWorld; };
 	void setLoadedWorldName(std::string name) { m_worldName = name; };
