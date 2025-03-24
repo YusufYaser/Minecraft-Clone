76bf6b53c8df21a15cff9a9fe73ca8cb7e00f973
Yusuf Kelany <me@yusufyaser.xyz>
2025-03-24 16:22:50 +0200

    add support for textures with no transparency

--- src/Textures/Textures.cpp
+++ src/Textures/Textures.cpp
@@ -184,7 +184,7 @@ TextureAtlas* initializeTextures() {
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, numCh == 4 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, data);
 			glGenerateMipmap(GL_TEXTURE_2D);
 		}
 
